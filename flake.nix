48db6f5e068fffb7a1084acdbc788d98cd908c9a
Dan Nixon <dan@dan-nixon.com>
2026-01-16 12:25:25 +0000

    chore: switch from devenv to nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # Toolchain
+        rustup
+        espup
+
+        # Upload/debug tool
+        probe-rs-tools
+
+        # Code formatting tools
+        treefmt
+        alejandra
+        mdl
+        rustfmt
+
+        # Release tools
+        release-plz
+      ];
+
+      DBX_CONTAINER_IMAGE = "ubuntu:24.04";
+      DBX_CONTAINER_NAME = "tildagon-rs-build-environment";
+    };
+  };
+}
